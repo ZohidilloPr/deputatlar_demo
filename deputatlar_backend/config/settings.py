51c3d4187d1afb7720bbc9593ea12fcbfdef11db
ZohidilloPr <turgunovzohidillopr@yahoo.com>
2022-05-09 18:36:22 +0500

    main

--- deputatlar_backend/config/settings.py
+++ deputatlar_backend/config/settings.py
@@ -32,14 +32,23 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #local apps
     'app.apps.AppConfig',
+    #global apps
 ]
+
+REST_FRAMEWORKS = {
+    'DEFAULT_PERMISSION_CLASSES':[
+        'rest_framework.permissions.AllowAny'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
